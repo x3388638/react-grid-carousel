b9ea4925d08c8f741a5d14fe4b3b67f59782ba2f
YY <z3388638@gmail.com>
2020-01-21 22:49:47 +0800

    Init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', 'minify'],
+            comments: false
+          }
+        }
+      }
+    ]
+  }
+}
